e5123d0d1551fad64c91f2aec592110ef61cccd9
Nelson <nlopez-2019450@kinal.edu.gt>
2023-05-11 15:01:39 -0600

    Se agregó la base del FrontEnd

---
+++ FrontEnd/Gestor de Hoteles/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const imprimir = async(e) => {
+      e.preventDefault();
+      
+  }
+}
